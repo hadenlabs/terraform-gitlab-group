a23507493ae3fa9c269a64276ae94e1688790537
luismayta <luis@hadenlabs.com>
2021-09-27 13:02:06 -0500

    chore: implement variables group (#7)

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,13 @@ output "path" {
   description = "output path group"
   value       = local.outputs.path
 }
+
+output "variable" {
+  description = "output instance gitlab variables group"
+  value       = gitlab_group_variable.this
+}
+
+output "variables" {
+  value       = [for variable in gitlab_group_variable.this : variable.key]
+  description = "List of variables available."
+}
--- variables.tf
+++ variables.tf
@@ -2,3 +2,9 @@ variable "path" {
   type        = string
   description = "The full path of the group."
 }
+
+variable "variables" {
+  type        = map(any)
+  description = "Variables for group."
+  default     = {}
+}
