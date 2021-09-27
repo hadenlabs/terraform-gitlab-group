0a32ca22857526152aff5b47fc820cf9a52c30f7
luismayta <luis@hadenlabs.com>
2021-09-27 11:16:11 -0500

    chore: implement make group gitlab (#3)

--- main.tf
+++ main.tf
@@ -1,1 +1,35 @@
+locals {
+  defaults = {}
 
+  input = {
+    name        = var.name
+    path        = var.name
+    description = var.description
+  }
+
+  generated = {
+    name        = local.input.name
+    path        = local.input.name
+    description = local.input.description
+  }
+
+  outputs = {
+    name        = local.generated.name
+    path        = local.generated.name
+    description = local.generated.description
+  }
+
+}
+
+resource "gitlab_group" "this" {
+  name        = local.outputs.name
+  path        = local.outputs.path
+  description = local.outputs.description
+
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes = [
+      description
+    ]
+  }
+}
